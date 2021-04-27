2ecb6456bea8bebfc19bdea090227b7868ef805f
Anna Lubawa <anialubawa@gmail.com>
2021-04-27 21:20:51 +0200

    Updated backgrounds in layouts

--- app/src/main/java/com/annalubawa/shoppinglists/ui/ArchivedShoppingListsFragment.kt
+++ app/src/main/java/com/annalubawa/shoppinglists/ui/ArchivedShoppingListsFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.annalubawa.shoppinglists.R
 import com.annalubawa.shoppinglists.databinding.FragmentArchivedShoppingListsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
